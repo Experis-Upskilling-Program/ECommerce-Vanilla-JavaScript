64ec660c0e6f3177f657f2ebaf47c2c49359651f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-08-31 15:36:32 -0500

    [+] index.html: Feature/Crear-Producto

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
+const LS_KEY_PRODUCTS = 'products';
+
 /** Definir los elementos del DOM que voy atacar */
-const $dataList = document.querySelector( '[data-product-list]' );
-console.log( $dataList );
+const 
+    $formProduct = document.querySelector( '[data-form-product]' ),
+    $dataList = document.querySelector( '[data-product-list]' );
+
+console.log( $formProduct );
 
 /** Dibuja en el DOM */
 const showData = () => {
     const allProducts = loadData();
+
+    $dataList.innerHTML = '';
 
     const $ulEl = document.createElement( 'ul' );
     $ulEl.setAttribute( 'class', 'product-list' );
@@ -22,8 +29,36 @@ const showData = () => {
     $dataList.appendChild( $ulEl );
 }
 
+/** Crear Producto */
+const createProduct = ( newProduct ) => {
+    const allProducts = loadData();
+
+    setData( [ newProduct, ...allProducts ] );
+    showData();
+    $formProduct.reset();
+}
+
+/** Actualice los datos del localStorage */
+const setData = ( item ) =>
+    localStorage.setItem( LS_KEY_PRODUCTS, JSON.stringify( item ) ); 
+
 /** Obtiene los datos del localStorage */
-const loadData = () => JSON.parse( localStorage.getItem( 'products' ) ) || []; 
+const loadData = () => JSON.parse( localStorage.getItem( LS_KEY_PRODUCTS ) ) || []; 
+
+/** Eventos */
+$formProduct.addEventListener( 'submit', ( event ) => {
+    event.preventDefault();
+
+    const product = {
+        name: document.querySelector( '[data-input-name]' ).value,
+        price: Number( document.querySelector( '[data-input-price]' ).value ),
+        quantity: Number( document.querySelector( '[data-input-quantity]' ).value ),
+        category: document.querySelector( '[data-input-category]' ).value
+    }
+
+    createProduct( product );
+});
+
 
 const init = () => {
     showData();
